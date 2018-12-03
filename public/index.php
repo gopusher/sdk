d51a5843fbe0f308113439c04a41d2d14c84a21d
sh7ning <sh7ning@qq.com>
2018-12-03 12:16:38 +0800

    简化sdk方便业务自定义

--- public/index.php
+++ public/index.php
@@ -2,16 +2,32 @@
 
 include __DIR__ . '/../vendor/autoload.php';
 
-
-
 $body = file_get_contents('php://input');
 if (! empty($body)) {
-    file_put_contents('/tmp/chat-rpc.log', $body . PHP_EOL, FILE_APPEND);
+    //file_put_contents('/tmp/chat-rpc.log', $body . PHP_EOL, FILE_APPEND);
 
     header('Content-Type: application/json; charset=utf-8');
-    $gopusher = new \Gopusher\Sdk\Gopusher();
-    echo $gopusher->notification([new \Gopusher\Sdk\Notification\StatusHandler()])
-        ->handleAndResponse($_SERVER['HTTP_USER_AGENT'], $body);
+
+    class Handler implements \Gopusher\Sdk\Notification\Handler {
+        public function checkToken($connId, $token, $nodeId)
+        {
+            file_put_contents('/tmp/chat-rpc.log', 'checkToken ' .$connId. $token. $nodeId . PHP_EOL, FILE_APPEND);
+        }
+
+        public function online($connId, $nodeId)
+        {
+            file_put_contents('/tmp/chat-rpc.log', 'online ' .$connId. $nodeId . PHP_EOL, FILE_APPEND);
+        }
+
+        public function offline($connId, $nodeId)
+        {
+            file_put_contents('/tmp/chat-rpc.log', 'offline ' .$connId. $nodeId . PHP_EOL, FILE_APPEND);
+        }
+    }
+
+    $notification = new \Gopusher\Sdk\Notification\Notification();
+    $notification->registerHandler(new Handler());
+    echo $notification->handleAndResponse($_SERVER['HTTP_USER_AGENT'], $body);
     exit;
 }
 
--- src/Rpc/Client.php
+++ src/Rpc/Client.php
@@ -1,16 +1,26 @@
 <?php
 
+namespace Gopusher\Sdk\Rpc;
+
+use Exception;
+
 class JsonRPC
 {
     /**
      * @var array
      */
-    private static $obj = [];
+    protected static $obj = [];
 
-    private function getConnect($host, $port)
+    /**
+     * @param $host
+     * @param $port
+     * @return mixed
+     * @throws Exception
+     */
+    protected function getConnect($host, $port)
     {
         $key = $host . ':' . $port;
-        if (!isset(self::$obj[$key])) {
+        if (! isset(self::$obj[$key])) {
             $conn = fsockopen($host, $port, $errno, $errstr, 3);
             if (! $conn) {
                 throw new \Exception('建立连接失败');
@@ -20,12 +30,16 @@ class JsonRPC
         return self::$obj[$key];
     }
 
+    /**
+     * @param $host
+     * @param $port
+     * @param $data
+     * @return mixed|null
+     * @throws Exception
+     */
     public function execute($host, $port, $data) {
         $conn = $this->getConnect($host, $port);
 
-        if (! $conn) {
-            throw new \Exception('连接不存在');
-        }
         $err = fwrite($conn, json_encode(array_merge($data, ['id' => 0])) . "\n");
 
         if ($err === false) {
@@ -40,6 +54,15 @@ class JsonRPC
         return json_decode($line,true);
     }
 
+    /**
+     * @param $host
+     * @param $port
+     * @param $token
+     * @param array $connections
+     * @param $msg
+     * @return mixed|null
+     * @throws Exception
+     */
     public function SendToConnections($host, $port, $token, array $connections, $msg)
     {
         $data = array(
@@ -51,9 +74,23 @@ class JsonRPC
             ]],
         );
 
-        return $this->execute($host, $port, $data);
+        $ret = $this->execute($host, $port, $data);
+
+        // if (! empty($ret['result']['error'])) {
+        //     throw new \Exception($ret['result']['error']);
+        // }
+
+        return $ret;
     }
 
+    /**
+     * @param $host
+     * @param $port
+     * @param $token
+     * @param $msg
+     * @return mixed|null
+     * @throws Exception
+     */
     public function Broadcast($host, $port, $token, $msg)
     {
         $data = array(
@@ -67,6 +104,14 @@ class JsonRPC
         return $this->execute($host, $port, $data);
     }
 
+    /**
+     * @param $host
+     * @param $port
+     * @param $token
+     * @param array $connections
+     * @return mixed|null
+     * @throws Exception
+     */
     public function CheckConnectionsOnline($host, $port, $token, array $connections)
     {
         $data = array(
@@ -80,6 +125,13 @@ class JsonRPC
         return $this->execute($host, $port, $data);
     }
 
+    /**
+     * @param $host
+     * @param $port
+     * @param $token
+     * @return mixed|null
+     * @throws Exception
+     */
     public function GetAllConnections($host, $port, $token)
     {
         $data = array(
@@ -92,6 +144,14 @@ class JsonRPC
         return $this->execute($host, $port, $data);
     }
 
+    /**
+     * @param $host
+     * @param $port
+     * @param $token
+     * @param array $connections
+     * @return mixed|null
+     * @throws Exception
+     */
     public function KickConnections($host, $port, $token, array $connections)
     {
         $data = array(
@@ -105,6 +165,13 @@ class JsonRPC
         return $this->execute($host, $port, $data);
     }
 
+    /**
+     * @param $host
+     * @param $port
+     * @param $token
+     * @return mixed|null
+     * @throws Exception
+     */
     public function KickAllConnections($host, $port, $token)
     {
         $data = array(
@@ -117,6 +184,13 @@ class JsonRPC
         return $this->execute($host, $port, $data);
     }
 
+    /**
+     * @param $host
+     * @param $port
+     * @param $token
+     * @return mixed|null
+     * @throws Exception
+     */
     public function GetNodeId($host, $port, $token)
     {
         $data = array(
@@ -129,6 +203,9 @@ class JsonRPC
         return $this->execute($host, $port, $data);
     }
 
+    /**
+     * destruct
+     */
     public function __destruct()
     {
         foreach (self::$obj as $conn) {
@@ -137,64 +214,30 @@ class JsonRPC
     }
 }
 
-/** 发消息 **/
-// if (count($argv) < 3) {
-//     throw new Exception('消息发送参数格式: php ./example.php msg ...to');
-// }
-// $client = new JsonRPC();
-// $r = $client->SendToConnections("message.demo.com", 8901, 'token', array_slice($argv, 2), $argv[1]);
-// var_export($r);
-// exit;
-/** 发消息 end **/
+//$client = new JsonRPC();
+//$r = $client->SendToConnections("message.demo.com", 8901, 'token', array_slice($argv, 2), $argv[1]);
+//var_export($r);
 
-/** 广播 **/
-// if (count($argv) < 2) {
-//     throw new Exception('消息发送参数格式: php ./example.php msg');
-// }
 // $client = new JsonRPC();
 // $r = $client->Broadcast("message.demo.com", 8901, 'token', $argv[1]);
 // var_export($r);
-// exit;
-/** 广播 end **/
 
-/** 检测是否在线 **/
-// if (count($argv) < 2) {
-//     throw new Exception('消息发送参数格式: php ./example.php ...connections');
-// }
 // $client = new JsonRPC();
 // $r = $client->CheckConnectionsOnline("message.demo.com", 8901, 'token', array_slice($argv, 1));
 // var_export($r);
-// exit;
-/** 检测是否在线 end **/
 
-/** 获取所有在线id **/
 // $client = new JsonRPC();
 // $r = $client->GetAllConnections("message.demo.com", 8901, 'token');
 // var_export($r);
-// exit;
-/** 检测是否在线 end **/
 
-/** kick conn **/
-// if (count($argv) < 2) {
-//     throw new Exception('消息参数格式: php ./example.php ...connections');
-// }
-//
 // $client = new JsonRPC();
 // $r = $client->KickConnections("message.demo.com", 8901, 'token', array_slice($argv, 1));
 // var_export($r);
-// exit;
-/** kick conn end **/
 
-/** kick all connections **/
 // $client = new JsonRPC();
 // $r = $client->KickAllConnections("message.demo.com", 8901, 'token');
 // var_export($r);
-// exit;
-/** kick all connections end **/
 
-/** kick all connections **/
 // $client = new JsonRPC();
 // $r = $client->GetNodeId("message.demo.com", 8901, 'token');
 // var_export($r);
-// exit;
-/** kick all connections end **/
